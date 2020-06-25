338a388e9a9843d906ac21946705311ace769bc1
Jack Palaia <jack@jackpalaia.com>
2020-06-25 18:34:39 -0400

    testing

--- testing/testing.cpp
+++ testing/testing.cpp
@@ -3,10 +3,9 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        if (i%2 == 0) cout << i << " ";
-    }
-    cout << "\n";
+  string s;
+  for (int i = 0; i < 1000; i++) {
+    s += "a";
+  }
+  cout << s;
 }
